cc0615fff2e7b569ad9c494d2589b9b3ce603dcb
Gabriella <gabriellaomenye@gmail.com>
2026-08-19 02:40:04 +0100

    up

--- backend/server.js
+++ backend/server.js
@@ -58,7 +58,8 @@ app.use(
             "http://127.0.0.1:5501",
             "http://localhost:5501",
             "http://127.0.0.1:3000",
-            "http://localhost:3000"
+            "http://localhost:3000",
+            "https://https://liftlog-mauve-eight.vercel.app"
         ],
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"]
